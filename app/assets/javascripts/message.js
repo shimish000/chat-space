92975722adc6e620213c4919187defdc55bb007c
shimish000 <57582823+shimish000@users.noreply.github.com>
2019-12-04 19:08:48 +0900

    edit　message.js

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,65 @@
+$(function(){
+  function buildHTML(message){
+    var image = ""
+    message.image ? image = `<img src="${message.image}">` :image = ""
+  
+      var html = `<div class="main-chat__box">
+                    <div class="main-chat__box__info">
+                      <div class="main-chat__box__info__name">
+                        ${message.user_name}
+                      </div>
+                      <div class="main-chat__box__info__date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="main-chat__box__sentence">
+                      <p class="lower-message__content">
+                        ${message.text}
+                      </p>
+                      ${image}
+                    </div>
+                  </div>`
+    // }else {
+    //   var html = `<div class="main-chat__box">
+    //                 <div class="main-chat__box__info">
+    //                   <div class="main-chat__box__info__name">
+    //                     ${message.user_name}
+    //                   </div>
+    //                   <div class="main-chat__box__info__date">
+    //                     ${message.created_at}
+    //                   </div>
+    //                 </div>
+    //                 <div class="main-chat__box__sentence">
+    //                   <p class="lower-message__content">
+    //                     ${message.text}
+    //                   </p>
+    //                 </div>
+    //               </div>`
+    // }
+    return html;
+  }
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: 'POST', 
+      data:formData,  
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.main-chat').append(html)
+      $('#message_content').val("")
+      $('.submit-btn').removeAttr("disabled");
+    })
+    .fail(function(){
+      alert('error')
+    })
+  
+  })
+})
